3f6b2140dab5012fe6a9833ef2569029fea0029c
Adrian Magdici <113631054+AdrianMagdici@users.noreply.github.com>
2026-01-18 09:32:37 +0300

    Refactor js.js for improved readability and structure

--- js.js
+++ js.js
@@ -10,10 +10,10 @@ audio.volume = 0.15;
 window.addEventListener("blur", () => {
     if(!found) {
         document.title = "Please, come back!";
-        favicon.setAttribute("href", "/assets/determination.ico");
+        favicon.setAttribute("href", "assets/determination.ico");
         setTimeout(function (){
             document.title = "I'll wait...";
-            favicon.setAttribute("href", "/assets/patience.ico");
+            favicon.setAttribute("href", "assets/patience.ico");
         }, 30000)
     }
 });
@@ -21,10 +21,10 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
     if(!found) {
         document.title = "Thank you!";
-        favicon.setAttribute("href", "/assets/kindness.ico");
+        favicon.setAttribute("href", "assets/kindness.ico");
         setTimeout(function (){
             document.title = doctitle;
-            favicon.setAttribute("href", "/assets/determination.ico");
+            favicon.setAttribute("href", "assets/determination.ico");
         }, 3000)
     }
 });
@@ -53,7 +53,7 @@ function foundbutton() {
     let p13 = document.getElementById("p13");
     let ramb = document.getElementById("ramb");
     setTimeout(function (){
-        favicon.setAttribute("href", "/assets/perseverance.ico");
+        favicon.setAttribute("href", "assets/perseverance.ico");
         document.title = "You did it!";
     }, 1000);
     p1.innerText = "Oi, are you pleased with the choices you've picked so far, luv'?";
@@ -69,7 +69,7 @@ function foundbutton() {
     p11.innerText = "";
     p12.innerText = "";
     p13.innerText = "";
-    audio.setAttribute("src", "/assets/shop.mp3");
+    audio.setAttribute("src", "assets/shop.mp3");
     audio.volume = 0.08;
     ramb.style.display = "flex";
     spacers.style.display = "none";
@@ -77,9 +77,10 @@ function foundbutton() {
 };
 
 function rambchange() {
-    rambimg.setAttribute("src", "/assets/RAMBafter.png");
+    rambimg.setAttribute("src", "assets/RAMBafter.png");
 };
 
 function rambchange2() {
-    rambimg.setAttribute("src", "/assets/RAMB.png");
+    rambimg.setAttribute("src", "assets/RAMB.png");
+
 };
